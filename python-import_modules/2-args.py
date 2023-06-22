990d0a235246ba3bb6afd061a48536c887a64911
estebangzzord <bambanini@gmail.com>
2023-06-22 15:29:42 -0700

    a

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,15 +4,12 @@ if __name__ == "__main__":
     i = len(sys.argv) - 1
 
     if i == 0:
-        print("{} arguments.".format(i))
+        print("{}.".format(i), end='')
     elif i == 1:
         print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
 
     if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
