7cf4af64aa422c855c4ecb3b6461aac332ec0add
estebangzzord <bambanini@gmail.com>
2023-06-28 11:20:35 -0700

    a

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,8 @@
-
-0;276;0c#!/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
         result = a / b
-    except Exception:
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
