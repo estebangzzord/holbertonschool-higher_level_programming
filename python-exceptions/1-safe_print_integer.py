2c1296b449dd35050dbfbe0eaa9364db2e29663c
estebangzzord <bambanini@gmail.com>
2023-06-28 11:07:06 -0700

    a

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end='')
-            count += 1
-    except IndexError:
-        pass
-
-    print()
-    return count
+        print("{:d}".format(value))
+    except:
+        return (False)
+    else:
+        return (True)
