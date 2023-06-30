afac7aaa814e03f0239a3c4abec0854be6ec854e
estebangzzord <bambanini@gmail.com>
2023-06-29 19:57:40 -0700

    a

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    length = 0
     count = 0
-
-    for i in range(x):
+    for i in my_list:
+        length += 1
+    for j in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
-            break
-        else:
-            count += 1
-
-    print()
-    return (count)
+            print("{}".format(my_list[j]), end="")
+        except IndexError as err:
+            print("")
+            return length
+        count += 1
+    print("")
+    return count
