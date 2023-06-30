f416d4e7e906d5d1a917113d288ac29b801209ce
estebangzzord <bambanini@gmail.com>
2023-06-29 19:52:26 -0700

    a

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Define a class 'Square' """
+
+
+class Square:
+    """ Empty 'Square' class """
+    def __init__(self, size=0):
+        """ Constructor.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of the square """
+        return self.__size * self.__size
