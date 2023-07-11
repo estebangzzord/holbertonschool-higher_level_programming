4ba6913d0f0230ae1dacefd3fef9f7c20ddd3180
estebangzzord <bambanini@gmail.com>
2023-07-10 20:49:43 -0700

    a

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-Module have a class that inherits from list class
-"""
+''' Module: 1-my_list
+'''
 
 
 class MyList(list):
-    """MyList class that inherits from list"""
+    ''' Represents a MyList
+    '''
 
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)
-        Args:
-            self: referes to instance of the class
-        """
-        new_list = self[:]
-        new_list.sort()
-        print(new_list)
+        '''
+        prints the list, but sorted
+        '''
+        print(sorted(self))
