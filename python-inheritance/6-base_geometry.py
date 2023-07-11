62b855488ace4b4bc09a2ca55ef976871d2d009e
estebangzzord <bambanini@gmail.com>
2023-07-10 19:45:44 -0700

    a

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+creating area function for the class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    def area(self):
+        """instance method for calculating area
+
+        Raises:
+            Exception: in case the area method are not implemented yet
+        """
+        raise Exception("area() is not implemented")
