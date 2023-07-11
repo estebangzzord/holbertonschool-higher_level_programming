a3e43673facfcdffc94750a7a8c2ea485d6fd3c6
estebangzzord <bambanini@gmail.com>
2023-07-10 19:57:46 -0700

    a

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+sub-class Square Module
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square sub-class
+    """
+    def __init__(self, size):
+        """The __init__ special method
+
+        Attributes:
+            size (int): size of square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """return width/height"""
+        return f"[Square] {self.__size}/{self.__size}"
